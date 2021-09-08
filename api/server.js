117d10c19ef67e1cc1c211d29f66a61698ef30ce
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-08 11:59:54 -0700

    initialtestcommit

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const SchemeRouter = require('./schemes/scheme-router.js');
 
-const server = express();
+const server = express()
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
